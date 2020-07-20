f55cd66fef5e6f318c77b7b4d052c5e8c5616899
julimancan <julian.m.bustos@gmail.com>
2020-07-20 19:28:08 -0400

    solved exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,31 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
+
+
 # Your code goes here ...
+### Exercise 6: One-to-many association
+
+@store1.employees.create(
+  first_name: "Khurram"
+  last_name: "Virani"
+  hourly_rate: 60
+)
+
+@store1.employees.create(
+  first_name: "Lionel"
+  last_name: "Messi"
+  hourly_rate: 14
+)
+
+@store2.employees.create(
+  first_name: "Lindsay"
+  last_name: "Lohan"
+  hourly_rate: 15
+)
+
+@store2.employees.create(
+  first_name: "Paris"
+  last_name: "Hilton"
+  hourly_rate: 13
+)
