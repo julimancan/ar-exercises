1e77a48933a526bc05f1f79e46e38a25e4538f9b
julimancan <julian.m.bustos@gmail.com>
2020-07-20 19:15:37 -0400

    solved exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,15 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+### Exercise 5: Calculations
+
+@annual_revenue_all_stores = Store.sum(:annual_revenue)
+puts "annual_revenue_all_stores: $#{@annual_revenue_all_stores}"
+
+@average_annual_revenue_all_stores = Store.average(:annual_revenue)
+puts "average_annual_revenue_all_stores: $#{@average_annual_revenue_all_stores}"
+
+
+@stores_with_more_1m = Store.where("annual_revenue >= '1000000'")
+@count_stores_more_1m = @stores_with_more_1m.count
+puts "There are #{@count_stores_more_1m} stores producing more than 1M"
