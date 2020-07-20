e8aa4d7bd3d44f5afd72ed5466addc8648cc670e
julimancan <julian.m.bustos@gmail.com>
2020-07-20 19:42:20 -0400

    fixed exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -14,25 +14,25 @@ puts "----------"
 ### Exercise 6: One-to-many association
 
 @store1.employees.create(
-  first_name: "Khurram"
-  last_name: "Virani"
+ first_name: "Khurram",
+  last_name: "Virani",
   hourly_rate: 60
 )
 
 @store1.employees.create(
-  first_name: "Lionel"
-  last_name: "Messi"
+  first_name: "Lionel",
+  last_name: "Messi",
   hourly_rate: 14
 )
 
 @store2.employees.create(
-  first_name: "Lindsay"
-  last_name: "Lohan"
+  first_name: "Lindsay",
+  last_name: "Lohan",
   hourly_rate: 15
 )
 
 @store2.employees.create(
-  first_name: "Paris"
-  last_name: "Hilton"
+  first_name: "Paris",
+  last_name: "Hilton",
   hourly_rate: 13
 )
