0454627dd54fa066fb8449c479bbc4406f9a74b8
julimancan <julian.m.bustos@gmail.com>
2020-07-20 17:49:57 -0400

    solved the second exercise

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,13 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+### Exercise 2: Update the first store
+
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+puts @store1.name
+@store1.name = "El Trapiche"
+@store1.save
+puts @store1.name
